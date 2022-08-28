fed8b464ed177517541c1de7416bef39e68aa4c5
Malo de Poret <72134718+Malodeporet@users.noreply.github.com>
2022-08-29 01:58:13 +0200

    front-end init

---
+++ client/src/components/Comments.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import styled from "styled-components";
+import Comment from "./Comment";
+import ChannelImg from "../img/channel.webp";
+
+const Container = styled.div``;
+
+const NewComment = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+
+const Avatar = styled.img`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+`;
+
+const Input = styled.input`
+  border: none;
+  border-bottom: 1px solid ${({ theme }) => theme.soft};
+  color: ${({ theme }) => theme.text};
+  background-color: transparent;
+  outline: none;
+  padding: 5px;
+  width: 100%;
+`;
+
+const Comments = () => {
+  return (
+    <Container>
+      <NewComment>
+        <Avatar src={ChannelImg} alt="channel image" />
+        <Input placeholder="Add a comment..." />
+      </NewComment>
+      <Comment />
+      <Comment />
+      <Comment />
+      <Comment />
+      <Comment />
+      <Comment />
+      <Comment />
+    </Container>
+  );
+};
+
+export default Comments;
